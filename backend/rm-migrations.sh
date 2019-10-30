fdd03ce1ffd549d71a8d85451cef725da608b7a3
Sotiris Nanopoulos <sotirisnan@gmail.com>
2019-10-30 01:20:45 -0700

    fix remove migrations script (#14)

--- backend/rm-migrations.sh
+++ backend/rm-migrations.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
+# This script hard resets the migrations of the app. Use carefully.
+find . -path "*/migrations/*.py" -not -name "__init__.py" -not -path "./.env/*" -delete
+find . -path "*/migrations/*.pyc" -not -path "./.env/*" -delete
 rm db.sqlite3

881642a6124a6af069e4da9e0a3fa9c1b9d124d4
davinci <sotirisnan@gmail.com>
2019-11-11 08:38:56 -0800

    another commit

--- backend/conduit/settings/development.py
+++ backend/conduit/settings/development.py
@@ -3,7 +3,7 @@ from conduit.settings.common import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'e^9s8q1bp*n$g@wrs46)0#!*g4@2dw1d8v=*=tu_iv-483h=^u'
